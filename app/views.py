802b7f229dc63d9d5993ea7a3eb81060390b7bc3
zakir <zakiralekperov1998@gmail.com>
2024-11-01 08:19:08 +0300

    Добавлены переменные для ввода данных в таблицу. Изменен стиль кнопок на форме вывода

--- app/views.py
+++ app/views.py
@@ -25,20 +25,133 @@ def form():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     # Получаем данные из формы
-    total_area = request.form['total_area']
-    lease_area = request.form['lease_area']
-    anchor_area = request.form['anchor_area']
-    rate_first_period = request.form['rate_first_period']
-    rate_second_period = request.form['rate_second_period']
-    rate_third_period = request.form['rate_third_period']
-    rate_fourth_period = request.form['rate_fourth_period']
-    market_area = request.form['market_area']
-    market_rate = request.form['market_rate']
-    operating_expenses = request.form['operating_expenses']
-    actual_expenses = request.form['actual_expenses']
-    underloading = request.form['underloading']
-    capitalization_coefficient = request.form['capitalization_coefficient']
-    discount_rate = request.form['discount_rate']
+    total_area = float(request.form['total_area']) #1. Общая площадь здания (кв. м.):
+    lease_area = float(request.form['lease_area']) #2. Аренднопригодная площадь (кв. м.):
+    anchor_area = float(request.form['anchor_area']) #3. Площадь якорных арендаторов (%):
+    rate_first_period = float(request.form['rate_first_period']) # 4. Ставка на первый период (руб.):
+    rate_second_period = float(request.form['rate_second_period']) #5. Ставка на второй период (руб.):
+    rate_third_period = float(request.form['rate_third_period']) #6. Ставка на третий период (руб.):
+    rate_fourth_period = float(request.form['rate_fourth_period']) #7. Ставка на четвертый период (руб.):
+    market_area = float(request.form['market_area']) #8. Площадь рыночных арендаторов (%):
+    market_rate = float(request.form['market_rate']) #9. Ставка рыночных арендаторов (руб.):
+    operating_expenses = float(request.form['operating_expenses']) #10. Операционные расходы арендатора (руб на 1 кв. м.):
+    actual_expenses = float(request.form['actual_expenses']) #11. Фактические расходы собственника (руб на 1 кв. м.):
+    underloading = float(request.form['underloading']) #12. Недозагрузка (%):
+    capitalization_coefficient = float(request.form['capitalization_coefficient']) #13. Коэффициент капитализации для реверсии (%):
+    discount_rate = float(request.form['discount_rate']) #14. Ставка дисконтирования (%):
+
+    # Перменные для конфигурации
+
+    value_1_1 = 0
+    value_1_2 = 0
+    value_1_3 = 0
+    value_1_4 = 0
+
+    value_2_1 = 0
+    value_2_2 = 0
+    value_2_3 = 0
+    value_2_4 = 0
+
+    value_3_1 = 0
+    value_3_2 = 0
+    value_3_3 = 0
+    value_3_4 = 0
+
+    value_4_1 = 0
+    value_4_2 = 0
+    value_4_3 = 0
+    value_4_4 = 0
+
+    value_5_1 = 0
+    value_5_2 = 0
+    value_5_3 = 0
+    value_5_4 = 0
+
+    value_6_1 = 0
+    value_6_2 = 0
+    value_6_3 = 0
+    value_6_4 = 0
+
+    value_7_1 = 0
+    value_7_2 = 0
+    value_7_3 = 0
+    value_7_4 = 0
+
+    value_8_1 = 0
+    value_8_2 = 0
+    value_8_3 = 0
+    value_8_4 = 0
+
+    value_9_1 = 0
+    value_9_2 = 0
+    value_9_3 = 0
+    value_9_4 = 0
+
+    value_10_1 = 0
+    value_10_2 = 0
+    value_10_3 = 0
+    value_10_4 = 0
+
+    value_11_1 = 0
+    value_11_2 = 0
+    value_11_3 = 0
+    value_11_4 = 0
+
+    value_12_1 = 0
+    value_12_2 = 0
+    value_12_3 = 0
+    value_12_4 = 0
+
+    value_13_1 = 0
+    value_13_2 = 0
+    value_13_3 = 0
+    value_13_4 = 0
+
+    value_14_1 = 0
+    value_14_2 = 0
+    value_14_3 = 0
+    value_14_4 = 0
+
+    value_15_1 = 0
+    value_15_2 = 0
+    value_15_3 = 0
+    value_15_4 = 0
+
+    value_16_1 = 0
+    value_16_2 = 0
+    value_16_3 = 0
+    value_16_4 = 0
+
+    value_17_1 = 0
+    value_17_2 = 0
+    value_17_3 = 0
+    value_17_4 = 0
+
+    value_18_1 = 0
+    value_18_2 = 0
+    value_18_3 = 0
+    value_18_4 = 0
+
+    value_19_1 = 0
+    value_19_2 = 0
+    value_19_3 = 0
+    value_19_4 = 0
+
+    value_20_1 = 0
+    value_20_2 = 0
+    value_20_3 = 0
+    value_20_4 = 0
+
+    value_21_1 = 0
+    value_21_2 = 0
+    value_21_3 = 0
+    value_21_4 = 0
+
+    value_22_1 = 0
+    value_22_2 = 0
+    value_22_3 = 0
+    value_22_4 = 0
+
 
     # Создаем список значений для таблицы
     results = [
@@ -46,197 +159,199 @@ def calculate():
         {"number": 1,
          "name": "Арендопригодная площадь:",
          "unit": "кв. м",
-         "value1": lease_area,
-         "value2": lease_area,
-         "value3": lease_area,
-         "value4": lease_area
+         "value1": value_1_1,
+         "value2": value_1_2,
+         "value3": value_1_3,
+         "value4": value_1_4
          },
 
         {"number": 2,
          "name": "Арендная ставка по действующему договору аренды:",
          "unit": "руб/кв.м. в год",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_2_1,
+         "value2": value_2_2,
+         "value3": value_2_3,
+         "value4": value_2_4
          },
 
         {"number": 3,
          "name": "Рыночная арендная ставка:",
          "unit": "руб/кв.м. в год",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_3_1,
+         "value2": value_3_2,
+         "value3": value_3_3,
+         "value4": value_3_4
          },
 
         {"number": 4,
          "name": "Возмещаемые операционные расходы",
          "unit": "руб/кв.м. (на 1 кв.м. аренднопригодной площади здания)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_4_1,
+         "value2": value_4_2,
+         "value3": value_4_3,
+         "value4": value_4_4
          },
 
         {"number": 5,
          "name": "Арендная ставка с учетом возмещаемых операционных расходов",
          "unit": "руб./кв.м.(для якорных арендаторов)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_5_1,
+         "value2": value_5_2,
+         "value3": value_5_3,
+         "value4": value_5_4
          },
 
         {"number": 6,
          "name": "Арендная ставка с учетом возмещаемых операционных расходов",
          "unit": "руб./кв.м.(рыночная)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_6_1,
+         "value2": value_6_2,
+         "value3": value_6_3,
+         "value4": value_6_4
          },
 
         {"number": 7,
          "name": "ПВД",
          "unit": "руб.(якорные арендаторы)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_7_1,
+         "value2": value_7_2,
+         "value3": value_7_3,
+         "value4": value_7_4
          },
 
         {"number": 8,
          "name": "ПВД",
          "unit": "руб. (рыночные ставки)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_8_1,
+         "value2": value_8_2,
+         "value3": value_8_3,
+         "value4": value_8_4
          },
 
         {"number": 9,
          "name": "Недозагрузка",
          "unit": "% (для площадей сдаваемых по рыночным ставкам)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_9_1,
+         "value2": value_9_2,
+         "value3": value_9_3,
+         "value4": value_9_4
          },
 
         {"number": 10,
          "name": "ДВД",
          "unit": "руб. (по рыночным ставкам)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_10_1,
+         "value2": value_10_2,
+         "value3": value_10_3,
+         "value4": value_10_4
          },
 
         {"number": 11,
          "name": "ДВД",
          "unit": "руб. (якорные арендаторы)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_11_1,
+         "value2": value_11_2,
+         "value3": value_11_3,
+         "value4": value_11_4
          },
 
         {"number": 12,
          "name": "ДВД",
          "unit": "руб. (всего)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_12_1,
+         "value2": value_12_2,
+         "value3": value_12_3,
+         "value4": value_12_4
          },
 
         {"number": 13,
          "name": "Операционные расходы",
          "unit": "руб./кв.м. в год (на кв.м. общей площади здания)",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_13_1,
+         "value2": value_13_2,
+         "value3": value_13_3,
+         "value4": value_13_4
          },
 
         {"number": 14,
          "name": "Операционные расходы",
          "unit": "руб. в год",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_14_1,
+         "value2": value_14_2,
+         "value3": value_14_3,
+         "value4": value_14_4
          },
 
         {"number": 15,
          "name": "ЧОД",
          "unit": "руб.",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_15_1,
+         "value2": value_15_2,
+         "value3": value_15_3,
+         "value4": value_15_4
          },
 
         {"number": 16,
          "name": "Ставка терминальной капитализации ",
          "unit": "%",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_16_1,
+         "value2": value_16_2,
+         "value3": value_16_3,
+         "value4": value_16_4
          },
 
         {"number": 17,
          "name": "Терминальная стоимость",
          "unit": "руб.",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_17_1,
+         "value2": value_17_2,
+         "value3": value_17_3,
+         "value4": value_17_4
          },
 
         {"number": 18,
          "name": "Ставка дисконтирования",
          "unit": "%. ",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_18_1,
+         "value2": value_18_2,
+         "value3": value_18_3,
+         "value4": value_18_4
         },
 
         {"number": 19,
          "name": "Фактор дисконтирования",
          "unit": "",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_19_1,
+         "value2": value_19_2,
+         "value3": value_19_3,
+         "value4": value_19_4
          },
 
         {"number": 20,
          "name": "Текущая стоимость денежного потока ",
          "unit": " руб.",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""},
+         "value1": value_20_1,
+         "value2": value_20_2,
+         "value3": value_20_3,
+         "value4": value_20_4
+         },
 
         {"number": 21,
          "name": "Рыночная стоимость ",
          "unit" : " руб.",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""
+         "value1": value_21_1,
+         "value2": value_21_2,
+         "value3": value_21_3,
+         "value4": value_21_4
          },
 
         {"number": 22, "name": "Рыночная стоимость ",
          "unit": " руб. (округленно) ",
-         "value1": "",
-         "value2": "",
-         "value3": "",
-         "value4": ""},
+         "value1": value_22_1,
+         "value2": value_22_2,
+         "value3": value_22_3,
+         "value4": value_22_4
+         },
     ]
 
 
