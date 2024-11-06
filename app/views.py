15db1a894a63ee599e3e0f84594de78b5bb4c9bd
SAiIT <zakiralekperov1998@gmail.com>
2024-11-06 21:08:35 +0300

    Никушка

--- app/views.py
+++ app/views.py
@@ -122,32 +122,32 @@ def calculate():
     value_16_3 = capitalization_coefficient
     value_16_4 = capitalization_coefficient
 
-    value_17_1 = value_15_4/ value_16_1
-    value_17_2 = value_15_4/ value_16_1
-    value_17_3 = value_15_4/ value_16_1
-    value_17_4 = value_15_4/ value_16_1
+    value_17_1 = value_15_4/ (value_16_1 /100)
+    value_17_2 = value_15_4/ (value_16_1 / 100)
+    value_17_3 = value_15_4/ (value_16_1 / 100)
+    value_17_4 = value_15_4/ (value_16_1 / 100)
 
-    value_18_1 = 0
-    value_18_2 = 0
-    value_18_3 = 0
-    value_18_4 = 0
+    value_18_1 = discount_rate
+    value_18_2 = discount_rate
+    value_18_3 = discount_rate
+    value_18_4 = discount_rate
 
-    value_19_1 = 0
-    value_19_2 = 0
-    value_19_3 = 0
-    value_19_4 = 0
+    value_19_1 = 1 / ((1 +value_18_1 / 100 )**1)
+    value_19_2 = 1 / ((1 +value_18_1 / 100 )**2)
+    value_19_3 = 1 / ((1 +value_18_1 / 100 )**3)
+    value_19_4 = None
 
-    value_20_1 = 0
-    value_20_2 = 0
-    value_20_3 = 0
-    value_20_4 = 0
+    value_20_1 =  value_15_1 * value_19_1
+    value_20_2 =  value_15_2 * value_19_2
+    value_20_3 =  value_15_3 * value_19_3
+    value_20_4 =  value_17_1 * value_19_3
 
-    value_21_1 = 0
-    value_21_2 = 0
-    value_21_3 = 0
-    value_21_4 = 0
+    value_21_1 = value_20_1 +  value_20_2 +  value_20_3 + value_20_4
+    value_21_2 = value_20_1 +  value_20_2 +  value_20_3 + value_20_4
+    value_21_3 = value_20_1 +  value_20_2 +  value_20_3 + value_20_4
+    value_21_4 = value_20_1 +  value_20_2 +  value_20_3 + value_20_4
 
-    value_22_1 = 0
+    value_22_1 = round(value_20_1 +  value_20_2 +  value_20_3 +  value_20_4)
     value_22_2 = 0
     value_22_3 = 0
     value_22_4 = 0
@@ -159,195 +159,195 @@ def calculate():
         {"number": 1,
          "name": "Арендопригодная площадь:",
          "unit": "кв. м",
-         "value1": value_1_1,
-         "value2": value_1_2,
-         "value3": value_1_3,
-         "value4": value_1_4
+         "value1": int(value_1_1),
+         "value2": int(value_1_2),
+         "value3": int(value_1_3),
+         "value4": int(value_1_4)
          },
 
         {"number": 2,
          "name": "Арендная ставка по действующему договору аренды:",
          "unit": "руб/кв.м. в год",
-         "value1": value_2_1,
-         "value2": value_2_2,
-         "value3": value_2_3,
-         "value4": value_2_4
+         "value1": int(value_2_1),
+         "value2": int(value_2_2),
+         "value3": int(value_2_3),
+         "value4": int(value_2_4)
          },
 
         {"number": 3,
          "name": "Рыночная арендная ставка:",
          "unit": "руб/кв.м. в год",
-         "value1": value_3_1,
-         "value2": value_3_2,
-         "value3": value_3_3,
-         "value4": value_3_4
+         "value1": int(value_3_1),
+         "value2": int(value_3_2),
+         "value3": int(value_3_3),
+         "value4": int(value_3_4)
          },
 
         {"number": 4,
          "name": "Возмещаемые операционные расходы",
          "unit": "руб/кв.м. (на 1 кв.м. аренднопригодной площади здания)",
-         "value1": value_4_1,
-         "value2": value_4_2,
-         "value3": value_4_3,
-         "value4": value_4_4
+         "value1": int(value_4_1),
+         "value2": int(value_4_2),
+         "value3": int(value_4_3),
+         "value4": int(value_4_4)
          },
 
         {"number": 5,
          "name": "Арендная ставка с учетом возмещаемых операционных расходов",
          "unit": "руб./кв.м.(для якорных арендаторов)",
-         "value1": value_5_1,
-         "value2": value_5_2,
-         "value3": value_5_3,
-         "value4": value_5_4
+         "value1": int(value_5_1),
+         "value2": int(value_5_2),
+         "value3": int(value_5_3),
+         "value4": int(value_5_4)
          },
 
         {"number": 6,
          "name": "Арендная ставка с учетом возмещаемых операционных расходов",
          "unit": "руб./кв.м.(рыночная)",
-         "value1": value_6_1,
-         "value2": value_6_2,
-         "value3": value_6_3,
-         "value4": value_6_4
+         "value1": int(value_6_1),
+         "value2": int(value_6_2),
+         "value3": int(value_6_3),
+         "value4": int(value_6_4)
          },
 
         {"number": 7,
          "name": "ПВД",
          "unit": "руб.(якорные арендаторы)",
-         "value1": value_7_1,
-         "value2": value_7_2,
-         "value3": value_7_3,
-         "value4": value_7_4
+         "value1": int(value_7_1),
+         "value2": int(value_7_2),
+         "value3": int(value_7_3),
+         "value4": int(value_7_4)
          },
 
         {"number": 8,
          "name": "ПВД",
          "unit": "руб. (рыночные ставки)",
-         "value1": value_8_1,
-         "value2": value_8_2,
-         "value3": value_8_3,
-         "value4": value_8_4
+         "value1": int(value_8_1),
+         "value2": int(value_8_2),
+         "value3": int(value_8_3),
+         "value4": int(value_8_4)
          },
 
         {"number": 9,
          "name": "Недозагрузка",
          "unit": "% (для площадей сдаваемых по рыночным ставкам)",
-         "value1": value_9_1,
-         "value2": value_9_2,
-         "value3": value_9_3,
-         "value4": value_9_4
+         "value1": int(value_9_1),
+         "value2": int(value_9_2),
+         "value3": int(value_9_3),
+         "value4": int(value_9_4)
          },
 
         {"number": 10,
          "name": "ДВД",
          "unit": "руб. (по рыночным ставкам)",
-         "value1": value_10_1,
-         "value2": value_10_2,
-         "value3": value_10_3,
-         "value4": value_10_4
+         "value1": int(value_10_1),
+         "value2": int(value_10_2),
+         "value3": int(value_10_3),
+         "value4": int(value_10_4)
          },
 
         {"number": 11,
          "name": "ДВД",
          "unit": "руб. (якорные арендаторы)",
-         "value1": value_11_1,
-         "value2": value_11_2,
-         "value3": value_11_3,
-         "value4": value_11_4
+         "value1": int(value_11_1),
+         "value2": int(value_11_2),
+         "value3": int(value_11_3),
+         "value4": int(value_11_4)
          },
 
         {"number": 12,
          "name": "ДВД",
          "unit": "руб. (всего)",
-         "value1": value_12_1,
-         "value2": value_12_2,
-         "value3": value_12_3,
-         "value4": value_12_4
+         "value1": int(value_12_1),
+         "value2": int(value_12_2),
+         "value3": int(value_12_3),
+         "value4": int(value_12_4)
          },
 
         {"number": 13,
          "name": "Операционные расходы",
          "unit": "руб./кв.м. в год (на кв.м. общей площади здания)",
-         "value1": value_13_1,
-         "value2": value_13_2,
-         "value3": value_13_3,
-         "value4": value_13_4
+         "value1": int(value_13_1),
+         "value2": int(value_13_2),
+         "value3": int(value_13_3),
+         "value4": int(value_13_4)
          },
 
         {"number": 14,
          "name": "Операционные расходы",
          "unit": "руб. в год",
-         "value1": value_14_1,
-         "value2": value_14_2,
-         "value3": value_14_3,
-         "value4": value_14_4
+         "value1": int(value_14_1),
+         "value2": int(value_14_2),
+         "value3": int(value_14_3),
+         "value4": int(value_14_4)
          },
 
         {"number": 15,
          "name": "ЧОД",
          "unit": "руб.",
-         "value1": value_15_1,
-         "value2": value_15_2,
-         "value3": value_15_3,
-         "value4": value_15_4
+         "value1": int(value_15_1),
+         "value2": int(value_15_2),
+         "value3": int(value_15_3),
+         "value4": int(value_15_4)
          },
 
         {"number": 16,
          "name": "Ставка терминальной капитализации ",
          "unit": "%",
-         "value1": value_16_1,
-         "value2": value_16_2,
-         "value3": value_16_3,
-         "value4": value_16_4
+         "value1": int(value_16_1),
+         "value2": int(value_16_2),
+         "value3": int(value_16_3),
+         "value4": int(value_16_4)
          },
 
         {"number": 17,
          "name": "Терминальная стоимость",
          "unit": "руб.",
-         "value1": value_17_1,
-         "value2": value_17_2,
-         "value3": value_17_3,
-         "value4": value_17_4
+         "value1": int(value_17_1),
+         "value2": int(value_17_2),
+         "value3": int(value_17_3),
+         "value4": int(value_17_4)
          },
 
         {"number": 18,
          "name": "Ставка дисконтирования",
          "unit": "%. ",
-         "value1": value_18_1,
-         "value2": value_18_2,
-         "value3": value_18_3,
-         "value4": value_18_4
+         "value1": int(value_18_1),
+         "value2": int(value_18_2),
+         "value3": int(value_18_3),
+         "value4": int(value_18_4)
         },
 
         {"number": 19,
          "name": "Фактор дисконтирования",
          "unit": "",
-         "value1": value_19_1,
-         "value2": value_19_2,
-         "value3": value_19_3,
-         "value4": value_19_4
+         "value1": round(value_19_1, 3),
+         "value2": round(value_19_2, 3),
+         "value3": round(value_19_3, 3),
+         "value4": ""
          },
 
         {"number": 20,
          "name": "Текущая стоимость денежного потока ",
          "unit": " руб.",
-         "value1": value_20_1,
-         "value2": value_20_2,
-         "value3": value_20_3,
-         "value4": value_20_4
+         "value1": int(value_20_1),
+         "value2": int(value_20_2),
+         "value3": int(value_20_3),
+         "value4": int(value_20_4)
          },
 
         {"number": 21,
          "name": "Рыночная стоимость ",
          "unit" : " руб.",
-         "value1": value_21_1,
-         "value2": value_21_2,
-         "value3": value_21_3,
-         "value4": value_21_4
+         "value1": round(value_21_1, 2),
+         "value2": round(value_21_2, 2),
+         "value3": round(value_21_3, 2),
+         "value4": round(value_21_4, 2)
          },
 
         {"number": 22, "name": "Рыночная стоимость ",
          "unit": " руб. (округленно) ",
-         "value1": value_22_1,
+         "value1": int(value_22_1),
          "value2": value_22_2,
          "value3": value_22_3,
          "value4": value_22_4
